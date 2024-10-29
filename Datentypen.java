154c96e9b29aad142a31679e3febb9c0b586dd1c
l95m <115934027+l95m@users.noreply.github.com>
2024-10-29 11:46:11 +0100

    Create Datentypen.java

---
+++ Datentypen.java
@@ -0,0 +1,45 @@
+public class Datentypen {
+
+	public static void main(String[] args) {
+	
+	// Ganze Zahlen
+		byte eineKleineZahl = ; 
+		System.out.println("byte: " + eineKleineZahl);			
+		
+		short wert = ;								 
+		System.out.println("short: " + wert);					
+		
+		int zahl = ;								
+		System.out.println("int: " + zahl);					
+		
+		long einLongWert = ;						
+		System.out.println("long: " + einLongWert);	
+			
+	// Gleitkomma Zahlen
+		
+		float einFloat = ;							
+		System.out.println("float: " + einFloat);				
+		
+		double kommaZahl = ;
+		System.out.println("double: " + kommaZahl);
+	
+	// char --> Zeichen Datentyp (char für Character)
+	
+		char einZeichen = ; 							
+		System.out.println("char Zeichen : " + einZeichen);
+		
+		char einZeichenMitUnicode = ;
+		System.out.println("char mit Unicode: " + einZeichenMitUnicode);
+		
+		char einZeichenMitDezimal = ;
+		System.out.println("char mit dezimal Zahl: " + einZeichenMitDezimal);
+		
+		char einZeichenMitBinaer = ;
+		System.out.println("char mit Dual Zahl: " + einZeichenMitBinaer);
+		
+		
+	// boolean - Wahrheits Datentyp
+		
+		boolean isTrue = ;
+		boolean isFalse = ;
+		System.out.println("boolean: " + isTrue + " / " + isFalse);
