13cbb5c8a5ccf81837fb079ffb32164b926ceb3b
l95m <115934027+l95m@users.noreply.github.com>
2024-11-05 10:59:09 +0100

    Create BieispielAufgabeA.java

---
+++ BieispielAufgabeA.java
@@ -0,0 +1,16 @@
+package beispiel;
+
+// a) Geben Sie mit einer for Schleife die Zahlen von 10 bis -10 aus (10, 9, 8...)
+
+public class BeispielAufgabeA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 10;i >= -10; i--) {
+			System.out.print(i + ", ");
+		}
+		
+	}
+
+}
